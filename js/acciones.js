edc10dc6b41649a31a5b41417368e7d7c331d698
Lanix 08 <Lanix 08>
2013-12-05 16:37:30 -0600

    CREA TABLAS
    CORREGIDO

--- js/acciones.js
+++ js/acciones.js
@@ -8,12 +8,41 @@ $(document).ready(function(e)
         
         $("#Crear").bind ("click", function (event)
          {                
-		 var db = openDatabase ("Test", "1.0", "Alumnos", 65535);
-		  db.transaction (function (ejecutar) 
-                  {
-                           alert ("base de datos creada");
-                  });//Ejecutar
-		     });//Crear
-		 $
+		 var db = openDatabase ("Test", "1.0", "base", 65535);
+		 alert ("base de datos Creada");
+		    });//Crear
+		 $("#Tabla").bind("click", function (event)
+		   {
+			 db.transaction(function (ejecutar)
+			   {
+				var SQL = "CREATE TABLE Alumnos (No_Control varchar(14) NOT NULL PRIMARY KEY, Nombre varchar(40) NOT NULL, ApellidoP varchar(30) NOT NULL, ApellidoM varchar(30), Grupo varchar(2)NOT NULL )";
+		    	ejecutar.executeSql (SQL, undefined, function () 
+					{ 
+      				alert ("Tabla Alumnos creada");
+    				}, error);//executesql
+				});
+				 db.transaction(function (ejecutar)
+			   		{
+					var SQL = "CREATE TABLE Grupos (Grupo varchar(2)NOT NULL, Activo integer, Asignatura varchar(10))";
+		    		ejecutar.executeSql (SQL, undefined, function () 
+			{ 
+      			alert ("Tabla Grupos creada");
+    		}, error);//executesql
+		   });
+		    db.transaction(function (ejecutar)
+			   {
+			var SQL = "CREATE TABLE Asignaturas (Asignatura varchar(10), NombreA Text )";
+		    ejecutar.executeSql (SQL, undefined, function () 
+			{ 
+      			alert ("Tabla Asignatura creada");
+    		}, error);//executesql
+  });
+		   });//Tabla
+		   function error (ejecutar, err)
+		   {
+			   alert ("Error de Base de Datos:" + err.message);
+			   return false;
+			   }//error
          }, false);//deviceready
 });//documen
+
